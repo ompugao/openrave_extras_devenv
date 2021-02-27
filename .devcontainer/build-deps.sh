9e7abfcd3dbd91915b92960d5b727284c8a62c6f
Shohei Fujii <fujii.shohei@gmail.com>
2021-02-27 22:58:13 +0800

    install new cmake earlier

--- .devcontainer/build-deps.sh
+++ .devcontainer/build-deps.sh
@@ -1,3 +1,4 @@
+set -e
 docker build -t ompugao/ros:melodic-desktop-full-cudnn8-gl -f Dockerfile.ros .
 docker build -t ompugao/openrave-deps:melodic-desktop-full-cudnn8-gl -f Dockerfile.openrave-deps --target dev --build-arg from=ompugao/ros:melodic-desktop-full-cudnn8-gl . 
 docker-compose --project-name openrave_extras_devenv -f ./docker-compose.yml build
