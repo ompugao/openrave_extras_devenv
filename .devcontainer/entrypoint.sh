9acd7122eb231f219378708c97cfeb78b65c150e
Shohei Fujii <fujii.shohei@gmail.com>
2021-02-27 19:00:40 +0800

    now things work!

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-set -e
+
+# fail immediately if we are in an interactive bash
+case $- in
+	*i*) ;;
+	*) set -e;;
+esac
+
 
 # setup ros environment
 source "/root/ros_catkin_ws/parent/install_isolated/setup.bash"
-source "/root/ros_catkin_ws/common/devel/setup.bash"
+source "/root/ros_catkin_ws/common_pkgs_deps/install_isolated/setup.bash"
+source "/root/ros_catkin_ws/common_pkgs/devel/setup.bash"
 source "/root/ros_catkin_ws/dev/devel/setup.bash"
+
 exec "$@"
