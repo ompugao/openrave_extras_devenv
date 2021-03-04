dd7cdc0f710d67166dd732d00a665a1c70954f66
Shohei Fujii <fujii.shohei@gmail.com>
2021-03-04 12:44:02 +0800

    minor update

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -13,4 +13,10 @@ source "/root/ros_catkin_ws/common_pkgs_deps/install_isolated/setup.bash"
 source "/root/ros_catkin_ws/common_pkgs/devel/setup.bash"
 source "/root/ros_catkin_ws/dev/devel/setup.bash"
 
+export OPENRAVE_PLUGINS=/usr/local/lib/openrave_extras_plugins/:$OPENRAVE_PLUGINS
+osr_openrave_dir=$(rospack find osr_openrave 2>/dev/null)
+if [ $? -eq 0 ]; then
+    export OPENRAVE_DATA=$OPENRAVE_DATA:$(rospack find osr_openrave)
+fi
+
 exec "$@"
