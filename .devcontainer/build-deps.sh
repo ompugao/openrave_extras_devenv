8780c428b9119827dc572bdc32485c1d3483a0aa
Shohei Fujii <fujii.shohei@gmail.com>
2021-03-09 15:31:15 +0800

    support multiple cuda version

--- .devcontainer/build-deps.sh
+++ .devcontainer/build-deps.sh
@@ -1,5 +1,34 @@
 set -e
-python ../gen_dockerfile.py Dockerfile.openrave Dockerfile.openrave.gen --OPENRAVE_BUILD_TYPE=Release --ROS_BUILD_TYPE=Release
-docker build -t ompugao/ros:melodic-desktop-full-cudnn8-gl -f Dockerfile.ros .
+export DOCKER_BUILDKIT=1
+export COMPOSE_DOCKER_CLI_BUILD=1
+python3 ../gen_dockerfile.py Dockerfile.openrave Dockerfile.openrave.gen --OPENRAVE_BUILD_TYPE=Release --ROS_BUILD_TYPE=Release
+
+# taken from https://github.com/cloudflare/semver_bash
+function semverParseInto() {
+    local RE='[^0-9]*\([0-9]*\)[.]\([0-9]*\)[.]\([0-9]*\)\([0-9A-Za-z-]*\)'
+    #MAJOR
+    eval $2=`echo $1 | sed -e "s#$RE#\1#"`
+    #MINOR
+    eval $3=`echo $1 | sed -e "s#$RE#\2#"`
+    #MINOR
+    eval $4=`echo $1 | sed -e "s#$RE#\3#"`
+    #SPECIAL
+    eval $5=`echo $1 | sed -e "s#$RE#\4#"`
+}
+
+CUDA_VERSION="${1:-11.0.3}"
+semverParseInto $CUDA_VERSION cuda_major cuda_minor cuda_patch cuda_special
+echo "$CUDA_VERSION -> M: $cuda_major m:$cuda_minor p:$cuda_patch s:$cuda_special"
+if [ $cuda_major -eq 10 ] && [ $cuda_minor -eq 2 ]; then
+	baseimage="nvidia/cuda:10.2-cudnn8-devel-ubuntu18.04"
+elif [ $cuda_major -eq 11 ] && [ $cuda_minor -eq 0 ]; then
+	baseimage="nvidia/cuda:11.0.3-cudnn8-devel-ubuntu18.04"
+else
+	echo "Unsupported CUDA Version: " $CUDA_VERSION
+	exit 1
+fi
+echo "baseimage: " $baseimage
+
+docker build -t ompugao/ros:melodic-desktop-full-cudnn8-gl -f Dockerfile.ros --build-arg from=$baseimage --progress=plain .
 docker build -t ompugao/openrave-deps:melodic-desktop-full-cudnn8-gl -f Dockerfile.openrave-deps --target dev --build-arg from=ompugao/ros:melodic-desktop-full-cudnn8-gl . 
 docker-compose --project-name openrave_extras_devenv -f ./docker-compose.yml build
---
+++ .devcontainer/install_libtorch.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+# taken from https://github.com/cloudflare/semver_bash
+function semverParseInto() {
+    local RE='[^0-9]*\([0-9]*\)[.]\([0-9]*\)[.]\([0-9]*\)\([0-9A-Za-z-]*\)'
+    #MAJOR
+    eval $2=`echo $1 | sed -e "s#$RE#\1#"`
+    #MINOR
+    eval $3=`echo $1 | sed -e "s#$RE#\2#"`
+    #MINOR
+    eval $4=`echo $1 | sed -e "s#$RE#\3#"`
+    #SPECIAL
+    eval $5=`echo $1 | sed -e "s#$RE#\4#"`
+}
+
+semverParseInto $CUDA_VERSION cuda_major cuda_minor cuda_patch cuda_special
+echo "$CUDA_VERSION -> M: $cuda_major m:$cuda_minor p:$cuda_patch s:$cuda_special"
+
+mkdir -p ~/3rdparty && cd ~/3rdparty
+# Install libtorch
+if [ $cuda_major -eq 10 ] && [ $cuda_minor -eq 2 ]; then
+	wget https://download.pytorch.org/libtorch/cu102/libtorch-cxx11-abi-shared-with-deps-1.7.1.zip
+	unzip libtorch-cxx11-abi-shared-with-deps-1.7.1.zip
+	rm libtorch-cxx11-abi-shared-with-deps-1.7.1.zip
+elif [ $cuda_major -eq 11 ] && [ $cuda_minor -eq 0 ]; then
+	wget https://download.pytorch.org/libtorch/cu110/libtorch-cxx11-abi-shared-with-deps-1.7.1%2Bcu110.zip
+	unzip libtorch-cxx11-abi-shared-with-deps-1.7.1+cu110.zip
+	rm libtorch-cxx11-abi-shared-with-deps-1.7.1+cu110.zip
+fi
+
