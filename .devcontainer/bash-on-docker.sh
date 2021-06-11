357e198251a64f3e15c4d4db96a88d6297fab50a
Shohei Fujii <fujii.shohei@gmail.com>
2021-06-12 00:06:15 +0800

    minor update

--- .devcontainer/bash-on-docker.sh
+++ .devcontainer/bash-on-docker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 CONTAINER=${1:-app}
+SCRIPTDIR=$(dirname $0)
+cd $SCRIPTDIR
 set -e
 bash ../set_xauth.sh
 docker-compose --project-name openrave_extras_devenv -f ./docker-compose.yml up -d
