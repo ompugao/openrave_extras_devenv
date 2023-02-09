d5080fddda4a5621edbe18363976070624bff9be
Shohei Fujii <fujii.shohei@gmail.com>
2023-02-09 13:14:33 +0800

    updated

--- .devcontainer/bash-on-docker.sh
+++ .devcontainer/bash-on-docker.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 CONTAINER=${1:-app}
 SCRIPTDIR=$(dirname $0)
+# DOCKERCOMPOSEYML=./docker-compose.old.yml
+DOCKERCOMPOSEYML=./docker-compose.yml
 cd $SCRIPTDIR
 set -e
 bash ../set_xauth.sh
-docker-compose --project-name openrave_extras_devenv -f ./docker-compose.yml up -d ${CONTAINER}
+docker compose --project-name openrave_extras_devenv -f $DOCKERCOMPOSEYML up -d ${CONTAINER}
 set +e
-docker-compose --project-name openrave_extras_devenv exec --workdir /workspace ${CONTAINER} bash
+docker compose --project-name openrave_extras_devenv -f $DOCKERCOMPOSEYML exec --workdir /workspace ${CONTAINER} bash
 
 
